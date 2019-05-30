36348e8a0aa8ebc589da4ca3ae491abd471a30d4
Andrew Miliy <a.miliy@cryptix.ag>
2019-05-30 21:24:56 +0200

    Service Fabric guest executables added

--- artifacts/geth/start_geth3.sh
+++ artifacts/geth/start_geth3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 IP=$(hostname -i)
 BOOTNODES=$(cat bootnodes.txt)
-ADDRESS=$(cat $HOSTNAME/address.txt)
+ADDRESS=$(cat n3/$HOSTNAME/address.txt)
+sleep 10
 geth --datadir n3/$HOSTNAME/ --syncmode full --port 30310 --rpc --rpcaddr $IP --rpcport 8555 --rpcapi 'personal,db,eth,net,web3,txpool,miner' --bootnodes $BOOTNODES --networkid 1999 --gasprice 1 --unlock $ADDRESS --password passphrase.txt --mine
